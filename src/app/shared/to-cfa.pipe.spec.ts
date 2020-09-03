0884af79a51d1bf7f0651b262076ad780b52cca2
honorezemagho <honorezemagho@gmail.com>
2020-09-03 15:15:51 +0100

    remove files

--- src/app/shared/to-cfa.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToCFAPipe } from './to-cfa.pipe';
-
-describe('ToCFAPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToCFAPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
